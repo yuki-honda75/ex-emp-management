d76484d155447b288317845394c7b976b4ed677c
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-07 13:12:05 +0900

    Adminのfromクラスを追加

---
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -0,0 +1,36 @@
+package jp.co.sample.form;
+
+public class InsertAdministratorForm {
+	/** 管理者名 */
+	private String name;
+	/** メールアドレス */
+	private String mailAddress;
+	/** パスワード */
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "InsertAdministratorForm [name=" + name
+				+ ", mailAddress=" + mailAddress
+				+ ", password=" + password + "]";
+	}
+}
