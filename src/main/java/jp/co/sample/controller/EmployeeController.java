dfe7a95195e57fcb035ce7a0b56ec2e518d943b4
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-07 15:58:12 +0900

    従業員更新情報を受け取るUpdateEmployeeForm作成

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -1,5 +1,15 @@
 package jp.co.sample.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author yuki
+ *
+ */
+@Controller
+@RequestMapping("")
 public class EmployeeController {
 
 }
