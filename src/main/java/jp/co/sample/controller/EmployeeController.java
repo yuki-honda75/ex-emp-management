ef753e8cc96f36f38d534f009d3d5beca983d120
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-07 16:03:35 +0900

    従業員リストの表示機能作成

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -1,7 +1,14 @@
 package jp.co.sample.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.service.EmployeeService;
 
 /**
  * 
@@ -9,7 +16,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  */
 @Controller
-@RequestMapping("")
+@RequestMapping("/employee")
 public class EmployeeController {
-
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@RequestMapping("/showList")
+	public String showList(Model model) {
+		List<Employee> employeeList = employeeService.showList();
+		model.addAttribute("employeeList", employeeList);
+		
+		return "employee/list";
+	}
 }
