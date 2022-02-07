99725c02f327678256d82323fd6cdc74b8c14d8a
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-07 17:17:59 +0900

    従業員詳細を表示する機能を追加

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -20,6 +22,11 @@ import jp.co.sample.service.EmployeeService;
 public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
+	
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
 	/**
 	 * 
 	 * @param model 従業員リストを格納するモデル
@@ -31,5 +38,13 @@ public class EmployeeController {
 		model.addAttribute("employeeList", employeeList);
 		
 		return "employee/list";
+	}
+	
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", employee);
+		
+		return "employee/detail";
 	}
 }
