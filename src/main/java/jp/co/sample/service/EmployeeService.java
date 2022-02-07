bd9ace5cadd6bf1408135dfd5f73200299baf400
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-07 15:54:07 +0900

    EmployeeServiceクラスの全件取得を定義

---
+++ src/main/java/jp/co/sample/service/EmployeeService.java
@@ -0,0 +1,26 @@
+package jp.co.sample.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.repository.EmployeeRepository;
+
+/**
+ * 
+ * @author yuki
+ *
+ */
+@Service
+@Transactional
+public class EmployeeService {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	public List<Employee> showList() {
+		return employeeRepository.findAll();
+	}
+}
