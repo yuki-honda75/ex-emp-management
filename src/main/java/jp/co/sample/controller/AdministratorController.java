04ed6f13b0570993722e6fa9a4e9198b66ff57a1
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-07 15:33:05 +0900

    ログイン処理をする機能追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,6 +24,9 @@ import jp.co.sample.service.AdministratorService;
 public class AdministratorController {
 	@Autowired
 	private AdministratorService administratorService;
+	
+	@Autowired
+	private HttpSession session;
 	/**
 	 * 
 	 * @return 従業員パラメータ
@@ -64,5 +70,17 @@ public class AdministratorController {
 	@RequestMapping("/")
 	public String toLogin() {
 		return "administrator/login";
+	}
+	
+	@RequestMapping("/login")
+	public String login(LoginForm form, Model model) {
+		Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+		if (administrator == null) {
+			model.addAttribute("error", "メールアドレスまたはパスワードが不正です。");
+			return toLogin();
+		}
+		
+		session.setAttribute("administratorName", administrator.getName());
+		return "forward:/employee/showList";
 	}
 }
--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -23,7 +23,12 @@ public class AdministratorService {
 	public void insert(Administrator administrator) {
 		administratorRepository.insert(administrator);
 	}
-	
+	/**
+	 * 
+	 * @param mailAddress メールアドレス
+	 * @param password パスワード
+	 * @return ログイン者情報
+	 */
 	public Administrator login(String mailAddress, String password) {
 		return administratorRepository.findByMailAddressAndPassword(mailAddress, password);
 	}
