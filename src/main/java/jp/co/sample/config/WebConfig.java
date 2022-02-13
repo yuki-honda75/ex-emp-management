7e27034ab92fed2c63bf864db062fcad66a86477
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-02-13 15:55:37 +0900

    ページング用の設定クラスWebConfigを作成

---
+++ src/main/java/jp/co/sample/config/WebConfig.java
@@ -0,0 +1,19 @@
+package jp.co.sample.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+		resolver.setOneIndexedParameters(true);
+		resolver.setMaxPageSize(10);
+		argumentResolvers.add(resolver);
+	}
+}
